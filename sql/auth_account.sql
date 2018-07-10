2156c8e19d7ae508b959336131fcf4b44785dc39
HimikoEmu <40901751+HimikoEmu@users.noreply.github.com>
2018-07-10 20:50:45 +0300

    Update auth_account.sql

--- sql/auth_account.sql
+++ sql/auth_account.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `account` ADD `avatar` INT(10) NULL DEFAULT '0' AFTER `remember_token`, ADD `silvercoins` INT(10) NULL DEFAULT '0' AFTER `avatar`, ADD `goldcoins` INT(10) NOT NULL DEFAULT '0' AFTER `silvercoins`, ADD `remember_token` varchar(50) NOT NULL AFTER `goldcoins`;
+ALTER TABLE `account` ADD `avatar` INT(10) NULL DEFAULT '0', ADD `silvercoins` INT(10) NULL DEFAULT '0' AFTER `avatar`, ADD `goldcoins` INT(10) NOT NULL DEFAULT '0' AFTER `silvercoins`, ADD `remember_token` varchar(50) NOT NULL AFTER `goldcoins`;
